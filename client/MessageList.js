9c9ba64ec1e730e27d24e34b2c8b6cc26a7831ec
Mariusz Galek <mgalek1@gmail.com>
2018-03-25 17:08:57 +0200

    Adjust message list display and styles

--- client/MessageList.js
+++ client/MessageList.js
@@ -1,27 +1,41 @@
-import React from 'react';
+import React, {Component} from 'react';
 import styles from './MessageList.css';
 
-const Message = props => (
-  <div className={styles.Message}>
-    <strong>{props.from} :</strong>
-    <span>{props.text}</span>
-  </div>
-);
+
+class Message extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            newText: this.props.text
+        };
+    }
+
+    render() {
+        return (
+            <div>
+                <div className={styles.Message}>
+                    <strong>{this.props.from} : </strong>
+                    <span className={styles.text}>{this.props.text}</span>
+                </div>
+            </div>
+        );
+    }
+};
 
 const MessageList = props => (
-  <div className={styles.MessageList}>
-    {
-      props.messages.map((message, i) => {
-        return (
-          <Message
-            key={i}
-            from={message.from}
-            text={message.text}
-          />
-        );
-      })
-    }
-  </div>
+    <div className={styles.MessageList}>
+        {
+            props.messages.map((message, i) => {
+                return (
+                    <Message
+                        key={i}
+                        from={message.from}
+                        text={message.text}
+                    />
+                );
+            })
+        }
+    </div>
 );
 
 export default MessageList;
